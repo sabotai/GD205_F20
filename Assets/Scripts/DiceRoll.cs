2a397b3f3a5d9ef28a7e6af4b719ed3ebc315dd3
Juno AM <sabotaione@gmail.com>
2020-09-16 16:09:02 -0400

    added week 4

---
+++ Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoll : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)){
+        	//Debug.Log("You rolled a " + dieRoll());
+        	Debug.Log("You rolled a " + diceRoll(3));
+        }
+    }
+
+    int dieRoll(){
+    	int dieValue = Random.Range(1,7);
+    	return dieValue;
+    }
+
+    int diceRoll(int howManyDice){
+    	int diceValue = 0;
+
+    	for(int i = 0; i < howManyDice; i++){
+    		diceValue += Random.Range(1,7);
+    	}
+
+    	return diceValue;
+    }
+}
--- Assets/Scripts/GridMovementTest.cs
+++ Assets/Scripts/GridMovementTest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridMovement : MonoBehaviour
+public class GridMovementTest : MonoBehaviour
 {
 	//create these public objects in our inspector so we can assign them there
 	//we assigned playerTransform to be our Player GameObject's transform
@@ -14,14 +14,14 @@ public class GridMovement : MonoBehaviour
   AudioSource speaker;
 
   public Transform block;
-  Vector3 newPosition;
-  public Material trailMat;
   public Transform fieldParent;
+  public Material pathMat;
+
+
 
 	//we assigned these to be equivalent in each direction, so fwd = (x = 0, y = 0, z = 2) and bwd = (x = 0, y = 0, z = -2)
 	public Vector3 fwd, bwd, lft, rgt, up, dwn;
   //public GameObject cam1, cam2;
-
 
     // Start is called before the first frame update
     // example of github tracking
@@ -35,40 +35,28 @@ public class GridMovement : MonoBehaviour
     void Update()
     {
 
+      Vector3 newPosition = playerTransform.position;
 
-      controls();
-      updatePosition();
-      checkHazards();
-      createTrail();
-
-
-    }
-
-
-    public void controls(){
-
-      newPosition = playerTransform.position; //current position
-
-      //access the Input class method called GetKeyDown, which takes a KeyCode parameter
-      //This will return true when this specific key is pressed down and false when it isn't
-      if(Input.GetKeyDown(KeyCode.W)){
-        //Offset the player's position by fwd amount, which we set in the inspector
-          newPosition = playerTransform.position + fwd;
+    	//access the Input class method called GetKeyDown, which takes a KeyCode parameter
+    	//This will return true when this specific key is pressed down and false when it isn't
+    	if(Input.GetKeyDown(KeyCode.W)){
+    		//Offset the player's position by fwd amount, which we set in the inspector
+       		newPosition = playerTransform.position + fwd;
           //cam1.SetActive(true);
           //cam2.SetActive(false);
-      }
+   		}
 
-      //do the same thing for the other keys
-      if(Input.GetKeyDown(KeyCode.S)){
-          newPosition = playerTransform.position + bwd;
+   		//do the same thing for the other keys
+    	if(Input.GetKeyDown(KeyCode.S)){
+       		newPosition = playerTransform.position + bwd;
           //cam2.SetActive(true);
           //cam1.SetActive(false);
-      }
-      if(Input.GetKeyDown(KeyCode.A)){
+   		}
+    	if(Input.GetKeyDown(KeyCode.A)){
 
-        //this is a simpler syntax for writing the same thing (+=)
-          newPosition += lft;
-      }
+    		//this is a simpler syntax for writing the same thing (+=)
+       		newPosition += lft;
+   		}
       if(Input.GetKeyDown(KeyCode.D)){
           newPosition += rgt;
       }
@@ -79,9 +67,6 @@ public class GridMovement : MonoBehaviour
           newPosition += up;
       }
 
-    }
-
-    public void updatePosition(){
 
       if (newPosition.y <= 4 && newPosition.y >= 0
         && newPosition.x >= 0 && newPosition.x <= 14
@@ -92,9 +77,7 @@ public class GridMovement : MonoBehaviour
          playerTransform.position = newPosition;
         }
       }
-    }
 
-    public void checkHazards() {
       for (int i = 0; i < hazards.Length; i++){
         if (playerTransform.position == hazards[i].position){
           playerTransform.position = initPos;
@@ -102,20 +85,18 @@ public class GridMovement : MonoBehaviour
         }
       }
 
+      
+
       if (playerTransform.position == hazard.position){
         playerTransform.position = initPos;
         speaker.PlayOneShot(deathClip, 0.6f);
       }
 
-    }
-
-    void createTrail(){
       for (int i = 0; i < fieldParent.childCount; i++){
-        if (playerTransform.position == fieldParent.GetChild(i).position){
-          fieldParent.GetChild(i).gameObject.GetComponent<Renderer>().material = trailMat;
-        }
+      	if (playerTransform.position == fieldParent.GetChild(i).position){
+      		fieldParent.GetChild(i).gameObject.GetComponent<Renderer>().material = pathMat;
+      	}
 
       }
     }
-
 }
