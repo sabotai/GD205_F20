87bb42b5d8d4254bd710ce068acd4abf7d8c780c
Juno AM <sabotaione@gmail.com>
2020-10-21 17:34:01 -0400

    week 8 stuff

---
+++ Assets/Scripts/KaliMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KaliMove : MonoBehaviour
+{
+
+	Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnim.SetInteger("moveState", 0);
+        if (Input.GetKey(KeyCode.W)){
+        	myAnim.SetInteger("moveState", 1);
+
+        	if (Input.GetKey(KeyCode.LeftShift)){
+        		myAnim.SetInteger("moveState", 2);
+        	}
+        }
+        if (Input.GetKeyDown(KeyCode.Space)){
+        	myAnim.SetInteger("moveState", -1);
+        } 
+    }
+}
---
+++ Assets/Scripts/RoboticMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboticMovement : MonoBehaviour
+{
+
+	Rigidbody rb;
+	public float forceAmt = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.AddForce(-transform.right * forceAmt);
+        rb.velocity += new Vector3(-1f * forceAmt,0f,0f);
+
+        if (transform.position.x < 350f){
+        	transform.position = new Vector3(350f, transform.position.y, transform.position.z);
+        	forceAmt *= -1f;
+
+        }
+    }
+}
