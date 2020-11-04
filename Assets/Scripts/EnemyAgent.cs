40c48c2e8a3b327a8d0cbf5ab6421ec9773d0491
Juno AM <sabotaione@gmail.com>
2020-11-04 17:02:39 -0500

    added week10 stuff

--- Assets/Scripts/EnemyAgent.cs
+++ Assets/Scripts/EnemyAgent.cs
@@ -6,25 +6,47 @@ using UnityEngine.AI;
 public class EnemyAgent : MonoBehaviour
 {
 	NavMeshAgent enemy;
+    public bool isPatrolling;
+    public Transform startPatrolPos, endPatrolPos;
+    public float destThresh = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
         enemy = GetComponent<NavMeshAgent>();
+        enemy.SetDestination(startPatrolPos.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPatrolling){
+            if (enemy.remainingDistance < destThresh){
+                Debug.Log("updating patrol position...");
+                Debug.Log("dest = " + enemy.destination);
+                Debug.Log("startPatrolPos = " + startPatrolPos.position);
+                if (Vector3.Distance(enemy.destination, startPatrolPos.position) < destThresh){
+                    enemy.SetDestination(endPatrolPos.position);
+                 } else if (Vector3.Distance(enemy.destination, endPatrolPos.position) < destThresh){
+                    enemy.SetDestination(startPatrolPos.position);
+                }
+
+            }
+        }
     }
 
     void OnTriggerStay(Collider triggerReport) {
     	
     	if (triggerReport.CompareTag("Player")){
+            isPatrolling = false;
     		Debug.Log("SUPPPPP (angry)");
     		enemy.SetDestination(triggerReport.transform.position);
     	}
+
+    }
+
+    void OnTriggerExit(Collider triggerReport) {
+       //isPatrolling = false;
 
     }
 }
